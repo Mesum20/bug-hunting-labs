08fb795e535460a65db5324ce86c1051e14a5c65
Mesum Raza <119757693+Mesum20@users.noreply.github.com>
2024-10-14 13:02:23 -0400

    Add files via upload

---
+++ fuzzing/db/backup.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (username, password) VALUES ('user1', 'simplepassword123');
+INSERT INTO users (username, password) VALUES ('guest', 'guest1234');
+INSERT INTO users (username, password) VALUES ('testuser', 'password123');
+INSERT INTO users (username, password) VALUES ('random', 'randompass');
+INSERT INTO users (username, password) VALUES ('admin', 'C0mpl3xP@ssw0rd!');
